d15e0de4dd6296807701f740ea5bd24399cf4eba
VikashAnandJha <mailtovikashjha@gmail.com>
2023-10-10 13:29:44 +0530

    removed actions

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -50,7 +50,7 @@ const LoginForm = () => {
                             type="text"
 
                             required
-                            className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                            className="appearance-none   relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="Username"
                             onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
 
